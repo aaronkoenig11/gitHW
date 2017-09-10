5d831ab731ef0c8478bdb835adfc779a8926e911
aaronkoenig11 <koenig.33@wright.edu>
2017-09-10 14:45:56 -0400

    Removed old Files

--- hello4110.java
+++
@@ -1,5 +0,0 @@
-class Hello4110{
-	public static void main(String[] args){
-		System.out.println("My Name is Aaron");
-	}
-}
--- hello4110.py
+++
@@ -1,3 +0,0 @@
-print "hello 4110! How are you?"
-print "I'm doing great!"
-print "Great!"
